f2449aa236edb398073cb37658c38f89fc5298e0
sisabel <sisabel2524@gmail.com>
2025-04-18 23:02:44 +0100

    renombre del proyecto

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'CesarMusic' title`, () => {
+  it(`should have the 'Sisabel' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('CesarMusic');
+    expect(app.title).toEqual('Sisabel');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, CesarMusic');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Sisabel');
   });
 });
