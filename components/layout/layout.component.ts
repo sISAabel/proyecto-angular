d8bf1b61e613e69c0650f3d2ab5f4641b54a9654
sisabel <sisabel2524@gmail.com>
2025-04-20 00:18:17 +0100

    creacion de la pantalla de contacto y se agrego multimedia a la we (audio de fondo)

--- components/layout/layout.component.ts
+++ components/layout/layout.component.ts
@@ -2,10 +2,11 @@ import { Component, Input, input } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { AppRoute } from '../../src/app/types/AppRoute';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-layout',
-  imports: [HeaderComponent, RouterModule],
+  imports: [HeaderComponent, RouterModule, CommonModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
@@ -13,5 +14,6 @@ export class LayoutComponent {
   @Input() src: string = ''
   @Input() alt: string = ''
   @Input() imageWidth: number = 20
+  @Input() mostrarBoton: boolean = true
   @Input() ruta: AppRoute = AppRoute.experiencia
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Sisabel';
+  audio!: HTMLAudioElement;
+
+  ngOnInit() {
+    if (typeof window !== 'undefined') {
+      this.audio = new Audio();
+      this.audio.src = 'assets/audio/dream-lofi.mp3';
+      this.audio.loop = true;
+      this.audio.volume = 0.1;
+
+      document.addEventListener('click', () => {
+        this.audio.play().catch(err => {
+          console.warn('Autoplay bloqueado:', err);
+        });
+      }, { once: true });
+    }
+  }
 }
+
