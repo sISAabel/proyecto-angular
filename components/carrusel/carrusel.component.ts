c05302df35ff26069b5c0e1dc24e170b02bc75d2
sisabel <sisabel2524@gmail.com>
2025-04-20 13:35:07 +0100

    correcciones a la marquesina, cambio de fondo general y pequeños ajustes de estilo. Eliminación de archivos no utilizados

--- components/carrusel/carrusel.component.ts
+++ components/carrusel/carrusel.component.ts
@@ -12,40 +12,40 @@ import { CommonModule } from '@angular/common';
 export class CarruselComponent {
   carruselItems = [
     {
-      subtitulo: 'primera version',
+      subtitulo: 'primera version | julio 2021',
       titulo: 'Portafolio',
       descripcion: 'Portafolio personal con un enfoque creativo y visualmente atractivo. Combina formas abstractas, colores vibrantes y un estilo moderno para mostrar habilidades digitales.',
-      boton: 'julio 2021',
+      boton: 'https://sisaabel.github.io/blog-personal-V4.1/',
     },
     {
-      subtitulo: 'tienda de electrónica',
+      subtitulo: 'tienda de electrónica | agosto 2021',
       titulo: 'Electro.',
       descripcion: 'Tienda online moderna de productos electrónicos, con una estructura clara que facilita la navegación. Ideal para destacar gadgets y tecnología de última generación.',
-      boton: 'agosto 2021',
+      boton: 'https://sisaabel.github.io/Electro./',
     },
     {
-      subtitulo: 'blog de gimnasio',
+      subtitulo: 'blog de gimnasio | agosto 2021',
       titulo: 'SISA-GYMS',
       descripcion: 'Web para un gimnasio, con una estética enérgica y urbana que motiva al usuario. El diseño refleja fuerza y dinamismo, ideal para promover un estilo de vida activo.',
-      boton: 'agosto 2021',
+      boton: 'https://sisaabel.github.io/SISA-GYMS/',
     },
     {
-      subtitulo: 'web de productividad',
+      subtitulo: 'web de productividad | agosto 2022',
       titulo: 'TuCalendi.com',
       descripcion: 'Un sistema de programación para reuniones y eventos para el web 3.0. TuCalendi es tu asistente personal online que trabaja para ti las 24 horas del día para llenar tu agenda y hacerte dueño de tu tiempo.',
-      boton: 'agosto 2022',
+      boton: 'https://www.tucalendi.com/es/',
     },
     {
-      subtitulo: 'blog de coctelería',
+      subtitulo: 'blog de coctelería | abril 2023',
       titulo: 'Drunky Ducks',
       descripcion: 'Landing page colorida y desenfadada para un proyecto creativo, relacionada a la coctelería. Su estética divertida y llamativa atrae al público joven y techie.',
-      boton: 'abril 2023',
+      boton: 'https://github.com/Drunky-Ducks',
     },
     {
-      subtitulo: 'despacho de abogados',
+      subtitulo: 'despacho de abogados | enero 2024',
       titulo: 'FU Abogados',
       descripcion: 'Página institucional para un despacho de abogados, con un diseño sobrio que transmite profesionalismo y confianza. Presenta sus servicios legales de forma directa y organizada.',
-      boton: 'enero 2024',
+      boton: 'https://funegrabogados.com/',
     },
   ];
 
